4a52d03911c05c5a97e2264cfcf1aabd8a89f020
JONATHAN DAVID CARVAJAL SULCA <jonathan.carvajal01@epn.edu.ec>
2019-05-20 17:42:02 -0500

    Creacion de rutas
    se emprezaron a crear las rutas

--- proyecto/FrontEnd/Blockfood/src/app/app-routing.module.ts
+++ proyecto/FrontEnd/Blockfood/src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from "./rutas/home/home.component";
+import {RutaMenuComponent} from "./rutas/ruta-menu/ruta-menu.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+
+
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'home',
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'menu',
+    component: RutaMenuComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+  }
+
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- proyecto/FrontEnd/Blockfood/src/app/app.module.ts
+++ proyecto/FrontEnd/Blockfood/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './rutas/home/home.component';
+import { RutaMenuComponent } from './rutas/ruta-menu/ruta-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RutaMenuComponent
   ],
   imports: [
     BrowserModule,
