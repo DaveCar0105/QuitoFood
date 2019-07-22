05665b1bb772deeb38ee2c4270a7db01c77bec8e
r0mell <42229321+r0mell@users.noreply.github.com>
2019-07-22 10:11:42 -0500

    json del blog
    recorrer el json desde el html

--- proyecto/FrontEnd/Blockfood/src/app/rutas/ruta-blog/ruta-blog.component.ts
+++ proyecto/FrontEnd/Blockfood/src/app/rutas/ruta-blog/ruta-blog.component.ts
@@ -9,12 +9,33 @@ export class RutaBlogComponent implements OnInit {
   saveBlog(){
     alert("Blog guardado con exito");
   }
+
+    /*blogs = [
+      {
+      autor:"rommel",
+      descripcion: "son las mejores papas",
+      puntuacion: 5
+      }, 
+      {
+      autor:"alex",
+      descripcion: "los mejores sanduches",
+      puntuacion: 10
+      },
+      {
+      autor:"david",
+      descripcion: "nada de malo en el restaurant",
+      puntuacion: 4
+      },
+  ]*/
+  blogs = ["pedro","juan","maria"]
+  
+  
   constructor() {
 
   }
 
   ngOnInit() {
-
+    
 
 
   }
