d3068b5331997a14edfc7ee362fcead8dcc125de
r0mell <42229321+r0mell@users.noreply.github.com>
2019-07-29 00:49:58 -0500

    correccion de stilos
    se anadio bootwatch para personalizar los estilos

--- proyecto/FrontEnd/Blockfood/src/app/rutas/ruta-blog/ruta-blog.component.ts
+++ proyecto/FrontEnd/Blockfood/src/app/rutas/ruta-blog/ruta-blog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import { BlogRestService } from 'src/app/services/blog-rest.service';
 import { Blog } from 'src/app/interfaces/blog';
 import { UsuarioRestService } from 'src/app/services/usuario-rest.service';
@@ -11,6 +11,7 @@ import { Usuario } from 'src/app/interfaces/user';
 })
 export class RutaBlogComponent implements OnInit {
 
+@HostBinding ('class') classes = 'row';
 
   blogs:any = [];
   
